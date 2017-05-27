096c6a3a4702ec4742af8227d33767bfdad51c58
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-27 09:42:52 +0200

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_scope :user do
+    authenticated :user do
+      root 'homepage#index', as: :authenticated_root
+    end
+
+    unauthenticated do
+    root 'devise/sessions#new', ad: :unauthenticated_root
+    end
+  end
 end

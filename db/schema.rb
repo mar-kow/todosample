2bde8e25767f34475b56a4f61192a9b045fcae2f
Marta Kowalska <marta.kowalska.987@gmail.com>
2017-05-27 12:45:03 +0200

    change schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -13,10 +13,12 @@
 ActiveRecord::Schema.define(version: 20170527075632) do
 
   create_table "tasks", force: :cascade do |t|
+    t.integer  "user_id"
     t.string   "title"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
